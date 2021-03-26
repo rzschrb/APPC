778e307dd43758bd6f9697969dcdd82f278f1cac
unknown <rzschrb@gmail.com>
2021-03-26 08:53:40 -0300

    replaced str.format() with f-strings

--- sequenceex/tempcalc.py
+++ sequenceex/tempcalc.py
@@ -23,7 +23,7 @@ if option == "1":  # Celsius > Fahrenheit
     try:
         c = float(input("\n> Digite a temperatura em Celsius que deseja converter: "))
         f = 9*c/5+32
-        print("{:.1f}".format(c), "graus Celsius correspondem a", "{:.1f}".format(f), "graus Fahrenheit\n")
+        print(f'{c:.1f} graus Celsius correspondem a {f:.1f} graus Fahrenheit\n')
         print("Obrigado por utilizar o programa")
     except ValueError:
         print("\nO valor informado não corresponde a uma temperatura válida")
@@ -31,7 +31,7 @@ elif option == "2":  # Fahrenheit > Celsius
     try:
         f = float(input("\n> Digite a temperatura em Fahrenheit que deseja converter: "))
         c = 5/9*(f-32)
-        print("{:.1f}".format(f), "graus Fahrenheit correspondem a", "{:.1f}".format(c), "graus Celsius\n")
+        print(f'{f:.1f} graus Fahrenheit correspondem a {c:.1f} graus Celsius\n')
         print("Obrigado por utilizar o programa")
     except ValueError:
         print("\nO valor informado não corresponde a uma temperatura válida")
@@ -39,7 +39,7 @@ elif option == "3":  # Celsius > Kelvin
     try:
         c = float(input("\n> Digite a temperatura em Celsius que deseja converter: "))
         k = c+275.15
-        print("{:.1f}".format(c), "graus Celsius correspondem a", "{:.2f}".format(k), "graus Kelvin\n")
+        print(f'{c:.1f} graus Celsius correspondem a {k:.2f} graus Kelvin\n')
         print("Obrigado por utilizar o programa")
     except ValueError:
         print("\nO valor informado não corresponde a uma temperatura válida")
@@ -47,7 +47,7 @@ elif option == "4":  # Kelvin > Celsius
     try:
         k = float(input("\n> Digite a temperatura em Kelvin que deseja converter: "))
         c = k-275.15
-        print("{:.2f}".format(k), "graus Kelvin correspondem a", "{:.1f}".format(c), "graus Celsius\n")
+        print(f'{k:.2f} graus Kelvin correspondem a {c:.1f} graus Celsius\n')
         print("Obrigado por utilizar o programa")
     except ValueError:
         print("\nO valor informado não corresponde a uma temperatura válida")
@@ -55,7 +55,7 @@ elif option == "5":  # Celsius > Rankine
     try:
         c = float(input("\n> Digite a temperatura em Celsius que deseja converter: "))
         r = (c+275.15)*1.8
-        print("{:.1f}".format(c), "graus Celsius correspondem a", "{:.2f}".format(r), "graus Rankine\n")
+        print(f'{c:.1f} graus Celsius correspondem a {r:.2f} graus Rankine\n')
         print("Obrigado por utilizar o programa")
     except ValueError:
         print("\nO valor informado não corresponde a uma temperatura válida")
@@ -63,7 +63,7 @@ elif option == "6":  # Rankine > Celsius
     try:
         r = float(input("\n> Digite a temperatura em Rankine que deseja converter: "))
         c = (r/1.8)-275.15
-        print("{:.2f}".format(r), "graus Rankine correspondem a", "{:.1f}".format(c), "graus Celsius\n")
+        print(f'{r:.2f} graus Rankine correspondem a {c:.1f} graus Celsius\n')
         print("Obrigado por utilizar o programa")
     except ValueError:
         print("\nO valor informado não corresponde a uma temperatura válida")
@@ -71,7 +71,7 @@ elif option == "7":  # Fahrenheit > Kelvin
     try:
         f = float(input("\n> Digite a temperatura em Fahrenheit que deseja converter: "))
         k = (f-32)*1.8+275.15
-        print("{:.2f}".format(f), "graus Fahrenheit correspondem a", "{:.2f}".format(k), "graus Kelvin\n")
+        print(f'{f:.2f} graus Fahrenheit correspondem a {k:.2f} graus Kelvin\n')
         print("Obrigado por utilizar o programa")
     except ValueError:
         print("\nO valor informado não corresponde a uma temperatura válida")
@@ -79,7 +79,7 @@ elif option == "8":  # Kelvin > Fahrenheit
     try:
         k = float(input("\n> Digite a temperatura em Kelvin que deseja converter: "))
         f = (k-275.15)*1.8+32
-        print("{:.2f}".format(k), "graus Kelvin correspondem a", "{:.2f}".format(f), "graus Fahrenheit\n")
+        print(f'{k:.2f} graus Kelvin correspondem a {f:.2f} graus Fahrenheit\n')
         print("Obrigado por utilizar o programa")
     except ValueError:
         print("\nO valor informado não corresponde a uma temperatura válida")
@@ -88,7 +88,7 @@ elif option == "9":  # Fahrenheit > Rankine
         f = float(input("\n> Digite a temperatura em Fahrenheit que deseja converter: "))
         c = 5/9*(f-32)
         r = (c + 275.15) * 1.8
-        print("{:.2f}".format(f), "graus Fahrenheit correspondem a", "{:.2f}".format(r), "graus Rankine\n")
+        print(f'{f:.2f} graus Fahrenheit correspondem a {r:.2f} graus Rankine\n')
         print("Obrigado por utilizar o programa")
     except ValueError:
         print("\nO valor informado não corresponde a uma temperatura válida")
@@ -97,7 +97,7 @@ elif option == "10":  # Rankine > Fahrenheit
         r = float(input("\n> Digite a temperatura em Rankine que deseja converter: "))
         c = (r/1.8)-275.15
         f = 9 * c / 5 + 32
-        print("{:.2f}".format(r), "graus Rankine correspondem a", "{:.2f}".format(f), "graus Fahrenheit\n")
+        print(f'{r:.2f} graus Rankine correspondem a {f:.2f} graus Fahrenheit\n')
         print("Obrigado por utilizar o programa")
     except ValueError:
         print("\nO valor informado não corresponde a uma temperatura válida")
@@ -106,7 +106,7 @@ elif option == "11":  # Kelvin > Rankine
         k = float(input("\n> Digite a temperatura em Kelvin que deseja converter: "))
         c = k-275.15
         r = (c + 275.15) * 1.8
-        print("{:.2f}".format(k), "graus Kelvin correspondem a", "{:.2f}".format(r), "graus Rankine\n")
+        print(f'{k:.2f} graus Kelvin correspondem a {r:.2f} graus Rankine\n')
         print("Obrigado por utilizar o programa")
     except ValueError:
         print("\nO valor informado não corresponde a uma temperatura válida")
@@ -115,7 +115,7 @@ elif option == "12":  # Rankine > Kelvin
         r = float(input("\n> Digite a temperatura em Rankine que deseja converter: "))
         c = (r / 1.8) - 275.15
         k = c + 275.15
-        print("{:.2f}".format(r), "graus Rankine correspondem a", "{:.2f}".format(k), "graus Kelvin\n")
+        print(f'{r:.2f} graus Rankine correspondem a {k:.2f} graus Kelvin\n')
         print("Obrigado por utilizar o programa")
     except ValueError:
         print("\nO valor informado não corresponde a uma temperatura válida")
